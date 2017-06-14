d8b335faf0566faa509fe8e434447d61ec8291fa
Mayra Azevedo <mayradazevedo@gmail.com>
2017-06-14 18:20:45 -0300

    mais um teste

--- hash_table/src/drive_hash.cpp
+++ hash_table/src/drive_hash.cpp
@@ -107,6 +107,10 @@ int main(){
     Account a;
     assert( hash_table.insert( a.get_key(), a) );
 
+    //Atualiza saldo de "Ringo Starr" 
+    Account acc(2, 10, 56789, 2500.25, "Ringo Starr");
+    hash_table.insert( acc.get_key(), acc);
+
     hash_table.print();
     std::cout << "\n>>>Passou em todos os testes!\n";
 
