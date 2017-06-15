b1a059d6f0954b735518a9a6c4406d625f88773a
Mayra Dantas <mayradazevedo@gmail.com>
2017-06-15 18:01:40 -0300

    testes com versão 3 das chaves

--- hash_table/src/drive_hash.cpp
+++ hash_table/src/drive_hash.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <cassert>
 #include <iomanip>
+#include <algorithm>
 
  struct Account{
     int bank_code;
@@ -11,6 +12,7 @@
     std::string name;
 
     using AcctKey = std::pair < std::string , int >;
+    using AcctKeyV3 = std::tuple < std::string , int , int , int >; //nome, banco, agencia e conta
 
     Account( int b=1, int ag=11, int ac=11111, float c=0.0, 
     std::string n="Anonimo")
@@ -20,6 +22,12 @@
     AcctKey get_key()
     {
         std::pair<std::string, int> p(name, account_code);
+        return p; 
+    }
+
+    AcctKeyV3 get_key_v3()
+    {
+        std::tuple<std::string, int, int, int> p(name, bank_code, branch_code, account_code);
         return p; 
     }
     
@@ -35,6 +43,7 @@
         return _os;
     }
 
+//=== Função de dispersão para versão 2
 struct KeyHash {
     std::size_t operator()( const Account::AcctKey & k_ ) const
     {
@@ -44,9 +53,38 @@ struct KeyHash {
     }
 };
 
+//=== Função de comparação para versao 2
 struct KeyEqual{
     bool operator()( const Account::AcctKey &lhs, const Account::AcctKey &rhs) const{
         return (lhs.first == rhs.first and lhs.second == rhs.second);
+    }
+};
+
+//== Função de dispersão para versão 3
+struct KeyHashV3 {
+    std::size_t operator()( const Account::AcctKeyV3 & k_ ) const
+    {
+        std::string message = "";
+
+        std::vector< std::string > keys = { std::get<0>(k_)
+            , std::to_string( std::get<1>(k_) )
+            , std::to_string( std::get<2>(k_) )
+            , std::to_string( std::get<3>(k_) ) };
+
+        std::sort(keys.begin(), keys.end() );
+
+        for( auto e: keys )
+            message += e;
+
+        return (std::hash <std::string >()( message )  ) ;
+    }
+};
+
+//== Função de comparação para versão 3
+struct KeyEqualV3{
+    bool operator()( const Account::AcctKeyV3 &lhs, const Account::AcctKeyV3 &rhs) const{
+        return (std::get<0>(lhs) == std::get<0>(rhs)  and std::get<1>(lhs)  == std::get<1>(rhs) 
+                and std::get<2>(lhs) == std::get<2>(rhs) and std::get<3>(lhs)  == std::get<3>(rhs) );
     }
 };
 
@@ -111,7 +149,57 @@ int main(){
     Account rec(2, 10, 56789, 2500.25, "Ringo Starr");
     hash_table.insert( rec.get_key(), rec);
 
+    std::cout << "Clientes na tabela da versão 2:\n";
     hash_table.print();
+    std::cout << std::endl;
+
+    ///////////////////////////////////
+    //                               //
+    // Teste com versao 3 das chaves //
+    //                               //
+    ///////////////////////////////////
+
+    HashTbl< Account::AcctKeyV3, Account, KeyHashV3, KeyEqualV3> hash_table_v3(5);
+    
+    //Teste empty()
+    assert( hash_table_v3.empty());
+    
+    //Teste insert()
+    assert( hash_table_v3.insert( accounts[0].get_key_v3(), accounts[0]) );
+    assert( hash_table_v3.empty() == false);
+    assert( hash_table_v3.count() == 1);
+
+    assert( hash_table_v3.insert( accounts[1].get_key_v3(), accounts[1]) );
+    assert( hash_table_v3.insert( accounts[2].get_key_v3(), accounts[2]) );
+    assert( hash_table_v3.insert( accounts[3].get_key_v3(), accounts[3]) );
+    assert( hash_table_v3.insert( accounts[4].get_key_v3(), accounts[4]) );
+
+    //Teste remove()
+    assert( hash_table_v3.remove( accounts[0].get_key_v3() ));
+    assert( hash_table_v3.count() == 4);
+
+    //Teste retrieve()
+    assert( hash_table_v3.retrieve( accounts[0].get_key_v3(), ac ) == false);
+    
+    assert( hash_table_v3.retrieve( accounts[3].get_key_v3(), ac ));
+
+    KeyEqualV3 cmp_v3;
+    assert( cmp_v3(ac.get_key_v3(), accounts[3].get_key_v3()) );
+
+    //Teste rehash()
+    assert( hash_table_v3.insert( accounts[5].get_key_v3(), accounts[5]) );
+
+    assert( hash_table_v3.insert( acc.get_key_v3(), acc) );
+
+    assert( hash_table_v3.insert( a.get_key_v3(), a) );
+
+    //Atualiza 
+    hash_table_v3.insert( rec.get_key_v3(), rec);
+
+    std::cout << "Clientes na tabela da versão 3:\n";
+    hash_table_v3.print();
+
+
     std::cout << "\n>>>Passou em todos os testes!\n";
 
     return 0;
