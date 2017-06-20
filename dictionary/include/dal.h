4335d9a0663a267c301d349a1ed8e9e8568e6702
Mayra Azevedo <mayradazevedo@gmail.com>
2017-06-20 08:20:43 -0300

    add comments

--- dictionary/include/dal.h
+++ dictionary/include/dal.h
@@ -22,7 +22,7 @@ class DAL{
         /*! \brief Método auxiliar de busca que recupera
          *         o elemento com chave 'k' e retorna seu índice. 
          *  \param k Chave do elemento que se deseja procurar.
-         *  \return Índice do elemento se o mesmo for encontrad, -1 caso contário.
+         *  \return Índice do elemento se o mesmo for encontrado, -1 caso contário.
          */ 
         int _search ( const Key & k ) const;
 
@@ -38,19 +38,24 @@ class DAL{
          *         com chave 'k' e guarda seu valor em 'info'. 
          *  \param k Chave do elemento que se deseja procurar.
          *  \param info Variável que guarda o elemento recuperado.
-         *  \return Verdadeiro se o elemento for encontrads, falso caso contário.
+         *  \return Verdadeiro se o elemento for encontrado, falso caso contário.
          */
         bool search ( const Key & d , DataType & info ) const ; 
         
-        bool insert ( const Key & new_key , const DataType & _newInfo ); 
+         /*! \brief Método que insere novo elemento no dicionário. 
+         *  \param new_key Chave do novo elemento.
+         *  \param new_info Valor do novo elemento.
+         *  \return Verdadeiro se a inserção for bem sucedida, falso caso contário.
+         */
+        bool insert ( const Key & new_key , const DataType & new_info ); 
         
         Key min ( ) const; 
         
         Key max ( ) const;
         
-        bool sucessor ( const Key & _x , Key & _y ) const;
+        bool sucessor ( const Key & k , Key & k_ ) const;
 
-        bool predecessor ( const Key & _x , Key & _y ) const;
+        bool predecessor ( const Key & k , Key & k_ ) const;
 
 
 };
